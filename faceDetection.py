5722317fdd07e7b10e73f4651e5e1256ad20ab9e
youdou-code <101744290+youdou-code@users.noreply.github.com>
2022-09-13 19:40:25 +0100

    Update faceDetection.py

--- faceDetection.py
+++ faceDetection.py
@@ -3,13 +3,18 @@ import numpy as np
 
 
 faceCascade = cv2.CascadeClassifier("Ress/haarcascade_frontalface_default.xml")
-img = cv2.imread("Ress/lena.png")
+cap = cv2.VideoCapture(1)
 # resized = cv2.resize(img,(400,700))
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-faces = faceCascade.detectMultiScale(img_gray,1.1,4)
- 
-for (x,y,w,h) in faces:
-    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
 
-cv2.imshow("img",img)
-cv2.waitKey(0)
+while True:
+    ret, img = cap.read()
+    img = cv2.flip(img, 1)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(img_gray,1.1,4)
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+
+    cv2.imshow("img",img)
+    if cv2.waitKey(10) == ord('q'):
+        break
